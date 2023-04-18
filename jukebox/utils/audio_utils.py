5d940735223c9304114ebf155d4d769aa7f5973b
FyreMael <fyremaelstrom@gmail.com>
2023-04-18 01:06:46 -0700

    Update to reflect librosa parameters

--- jukebox/utils/audio_utils.py
+++ jukebox/utils/audio_utils.py
@@ -29,7 +29,7 @@ def calculate_bandwidth(dataset, hps, duration=600):
         if isinstance(x, (tuple, list)):
             x, y = x
         samples = x.astype(np.float64)
-        stft = librosa.core.stft(np.mean(samples, axis=1), hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
+        stft = librosa.core.stft(np.mean(samples, axis=1), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
         spec = np.absolute(stft)
         spec_norm_total += np.linalg.norm(spec)
         spec_nelem += 1
